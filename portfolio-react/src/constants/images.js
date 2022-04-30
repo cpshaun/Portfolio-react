f2bafa36f694d0ebd2f79cc2dc2d127974753d63
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2022-04-30 13:58:31 +0200

    cleanup images

--- portfolio-react/src/constants/images.js
+++ portfolio-react/src/constants/images.js
@@ -1,55 +1,25 @@
-import AdobeXD from '../images/AdobeXD.svg';
 import Biome from '../images/Biome.png';
-import CSS from '../images/CSS.png';
 import displaypic from '../images/displaypic.jpg';
-import download from '../images/download.svg';
-import dp from '../images/dp.jpg';
 import DSO from '../images/DSO.jpeg';
-import email from '../images/email.svg';
-import Figma from '../images/Figma.png';
-import github from '../images/github.svg';
 import HDB_contact_info from '../images/HDB contact info.svg';
 import HDB from '../images/HDB.png';
-import HTML5 from '../images/HTML5.png';
-import Javascript from '../images/Javascript.png';
-import linkedin from '../images/linkedin.svg';
 import Logo from '../images/Logo.svg';
-import Nodejs from '../images/Nodejs.svg';
-import Python from '../images/Python.png';
-import React from '../images/React.png';
 import S from '../images/S.png';
 import Shaun from '../images/Shaun.svg';
-import Snowman from '../images/Snowman.svg';
 import star from '../images/star.gif';
 import Suibian from '../images/Suibian.png';
-import uparrow from '../images/upArrow.svg';
 import Virtual_Usher from '../images/Virtual Usher.svg';
 
 export default {
-    AdobeXD,
     Biome,
-    CSS,
     displaypic,
-    download,
-    dp,
     DSO,
-    email,
-    Figma,
-    github,
     HDB_contact_info,
     HDB,
-    HTML5,
-    Javascript,
-    linkedin,
     Logo,
-    Nodejs,
-    Python,
-    React,
     S,
     Shaun,
-    Snowman,
     star,
     Suibian,
-    uparrow,
     Virtual_Usher
 }
